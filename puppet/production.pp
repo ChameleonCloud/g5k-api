e09408a154a45911e41bcaf493e41de0fe4d6581
Pierre Riteau <priteau@uchicago.edu>
2015-03-11 19:20:04 +0000

    Add a simple production manifest

---
+++ puppet/production.pp
@@ -0,0 +1,24 @@
+# This puppet manifest is just here to configure a vanilla squeeze with the
+# required libs and configuration to serve as a production host for the
+# g5k-api software.
+class production {
+  include apt
+  include mysql
+  include git
+  include dpkg::dev
+
+  exec{ "create production database":
+    user => root, group => root,
+    require => Exec["allow mysql connections from all"],
+    command =>  "/usr/bin/mysql -e \"create database g5kapi_production CHARACTER SET utf8 COLLATE utf8_general_ci;\"",
+    creates => "/var/lib/mysql/g5kapi_production"
+  }
+}
+
+stage { "init": before  => Stage["main"] }
+
+class {"apt":
+  stage => init,
+}
+
+include production
